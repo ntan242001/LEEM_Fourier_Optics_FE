7e6943c523b63893a1c182d058592581fe9a4994
ntan242001 <62791920+ntan242001@users.noreply.github.com>
2021-06-28 22:22:44 +0800

    Update Aperture_Optimal_Resolution.py

--- Aperture_Optimal_Resolution.py
+++ Aperture_Optimal_Resolution.py
@@ -125,7 +125,7 @@ simulating_steps = 1 + 2**15 # total simulating steps
 x_array = (np.linspace(-object_size/2, object_size/2, simulating_steps) + object_size/simulating_steps)*1e-9
 
 # A function to choose different sample object function
-def create_object(object_type):
+def create_object(object_type, k = 1):
     global object_function, object_amplitude, object_phase
     if object_type == "Step amplitude object":
     # Creating an 1:1/sqrt(2) step amplitude object whose phase is uniformly set to 0
@@ -148,19 +148,30 @@ def create_object(object_type):
             
         object_amplitude = object_amplitude[::-1]
     
+    if object_type == "Step phase object":
+    # Creating a k.pi step phase object whose amplitude is uniformly set to 1        
+        object_amplitude = np.ones_like(x_array)
+        
+        object_phase = np.zeros_like(x_array)
+        
+        for counter, element in enumerate(x_array):
+            if element > 0:
+                object_phase[counter] = k * np.pi
+     
+    
     if object_type == "Error function phase object":
-    # Creating an error function phase object whose amplitude is uniformly set to 1         
+    # Creating an error function phase object whose amplitude is uniformly set to 1
         object_amplitude = np.ones_like(x_array)
         
         object_phase = np.ones_like(x_array)
         
         for counter, element in enumerate(x_array):
-            object_phase[counter] = (math.erf(element*1e9)+1)/2
+            object_phase[counter] = (math.erf(element*1e8)+1)/2*k*np.pi
         
     # Object function
     object_function = np.multiply(object_amplitude, np.exp(1j * object_phase)) 
 
-create_object("Step amplitude object")
+create_object("Error function phase object", k = 1)
 
 ##################################
 ##################################
