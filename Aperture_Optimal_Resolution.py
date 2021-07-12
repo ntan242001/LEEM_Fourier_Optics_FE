f2306532998fc3c5320b664bab7b05846e3b26d9
ntan242001 <62791920+ntan242001@users.noreply.github.com>
2021-07-12 19:17:22 +0800

    Update Aperture_Optimal_Resolution.py

--- Aperture_Optimal_Resolution.py
+++ Aperture_Optimal_Resolution.py
@@ -9,13 +9,13 @@ import math
 ######### Preamble ###########
 ##############################
 
-t_0 = time.time()
 
 # A function to choose different LEEM parameters
-def choose_LEEM_type(LEEM_type_str, aberration_corrected = True):
+def choose_LEEM_type(LEEM_type_str, aberration_corrected_bool = False):
     global E, E_0, C_c, C_cc, C_3c, C_3, C_5, alpha_ap, alpha_ill, \
-        delta_E, M_L, lamda, lamda_0, q_ill, q_ap, LEEM_type, E_0_series
+        delta_E, M_L, lamda, lamda_0, q_ill, q_ap, LEEM_type, aberration_corrected
     LEEM_type = LEEM_type_str
+    aberration_corrected = aberration_corrected_bool
     
     if LEEM_type == "IBM":
         if aberration_corrected == False:
@@ -34,7 +34,7 @@ def choose_LEEM_type(LEEM_type_str, aberration_corrected = True):
             
             delta_E = 0.25  # eV  Energy Spread
             M_L = 0.653  # Lateral Magnification
-            print("IBM NAC chosen.")
+            print("IBM nac chosen.")
             
         elif aberration_corrected == True:
             E = 15010  # eV  Nominal Energy After Acceleration
@@ -52,13 +52,13 @@ def choose_LEEM_type(LEEM_type_str, aberration_corrected = True):
         
             delta_E = 0.25  # eV  Energy Spread
             M_L = 0.653  # Lateral Magnification
-            print("IBM AC chosen.")
+            print("IBM ac chosen.")
             
         lamda = 6.6261e-34 / np.sqrt(2 * 1.6022e-19 * 9.1095e-31 * E)
         lamda_0 = 6.6261e-34 / np.sqrt(2 * 1.6022e-19 * 9.1095e-31 * E_0)
     
-        q_ap = alpha_ap / lamda
-        q_ill = alpha_ill / lamda
+        q_ap = alpha_ap/lamda
+        q_ill = alpha_ill/lamda
         
     elif LEEM_type == "Energy dependent":
         if aberration_corrected == False:
@@ -77,9 +77,10 @@ def choose_LEEM_type(LEEM_type_str, aberration_corrected = True):
             alpha_ill = 0.1e-3  # rad Illumination divergence angle
             M_L = 0.653  # Lateral Magnification
             
-            lamda = 6.6261e-34 / np.sqrt(2 * 1.6022e-19 * 9.1095e-31 * E) #in metre
-            q_ill = alpha_ill / lamda
-            print("Custom nac LEEM at " + str(E_0) + "eV chosen.")
+            lamda = 6.6261e-34 / np.sqrt(2 * 1.6022e-19 * 9.1095e-31 * E) # in metre
+            alpha_ap = (lamda/C_3)**(1/4) # rad Aperture angle for optimal resolution
+            
+            print("Custom nac LEEM at E_0 = " + str(E_0) + " eV chosen.")
             
         if aberration_corrected == True:
             E = 15010  # eV  Nominal Energy After Acceleration
@@ -97,14 +98,19 @@ def choose_LEEM_type(LEEM_type_str, aberration_corrected = True):
             alpha_ill = 0.1e-3  # rad Illumination divergence angle
             M_L = 0.653  # Lateral Magnification
             
-            lamda = 6.6261e-34 / np.sqrt(2 * 1.6022e-19 * 9.1095e-31 * E) #in metre
-            q_ill = alpha_ill / lamda
-            print("Custom nac LEEM at " + str(E_0) + "eV chosen.")    
+            lamda = 6.6261e-34 / np.sqrt(2 * 1.6022e-19 * 9.1095e-31 * E) # in metre
+            alpha_ap = (3/2*lamda/C_5)**(1/6) # rad Aperture angle for optimal resolution
+            
+            print("Custom ac LEEM at E_0 = " + str(E_0) + " eV chosen.")   
+        
+        lamda_0 = 6.6261e-34 / np.sqrt(2 * 1.6022e-19 * 9.1095e-31 * E_0) # in metre
+        
+        q_ap = alpha_ap/lamda
+        q_ill = alpha_ill/lamda
 
-choose_LEEM_type("IBM", aberration_corrected = False)
 
 # A function to set different defocus values
-def choose_defocus(defocus_type):
+def choose_defocus(defocus_type, value = 0):
     global delta_z
     if defocus_type == "In-focus":
         delta_z = 0
@@ -115,14 +121,15 @@ def choose_defocus(defocus_type):
     elif defocus_type == "A-Phi Scherzer defocus":
         delta_z = np.sqrt(9/64*C_5*lamda**2)
         print("A-Phi Scherzer defocus chosen.")
-
-choose_defocus("In-focus")
+    elif defocus_type == "custom":
+        delta_z = value
 
 
 object_size = 400               # simulating object size in nm
 simulating_steps = 1 + 2**15    # total simulating steps
 # An array of points in the x space
 x_array = (np.linspace(-object_size/2, object_size/2, simulating_steps) + object_size/simulating_steps)*1e-9
+
 
 # A function to choose different sample object function
 def create_object(object_type_str, k = 1):
@@ -168,11 +175,14 @@ def create_object(object_type_str, k = 1):
         
         for counter, element in enumerate(x_array):
             object_phase[counter] = (math.erf(element*1e8)+1)/2*k*np.pi
-        
+
     # Object function
     object_function = np.multiply(object_amplitude, np.exp(1j * object_phase)) 
+    print(object_type + " created")
 
-create_object("Error function phase object", k = 1)
+choose_LEEM_type("IBM", aberration_corrected_bool = False)
+choose_defocus("In-focus")
+create_object("Step phase object", k = 1)
 
 ##################################
 ######## End of Preamble #########
@@ -184,11 +194,12 @@ create_object("Error function phase object", k = 1)
 ########### Main Part ############
 ##################################
 print("Simulation start.")
+t_0 = time.time()
 # The object image is reversed through the lens
 object_function_reversed = object_function[::-1] 
     
 # Creating an array of different cut-off frequencies
-alpha_ap_series = np.linspace(0.5*1e-3, 1*1e-3, 20)
+alpha_ap_series = np.linspace(0.5*1e-3, 4*1e-3, 10)
 q_ap_series = alpha_ap_series/lamda
 
 # Initialising the series of function I(x) at different values of q_ap
@@ -251,7 +262,7 @@ matrixI = np.abs(matrixI)
 print('Simulation finished.')
 t_1 = time.time()
 
-print('Total time:' + str(round((t_1-t_0)/60, 3)) + 'minutes')
+print('Total time: ' + str(round((t_1-t_0)/60, 3)) + ' minutes')
 
 ##################################
 ######## End of Main Part ########
@@ -266,62 +277,146 @@ print('Total time:' + str(round((t_1-t_0)/60, 3)) + 'minutes')
 
 # Finding a list of resolutions corresponding to different aperture angles
 resolution_list = []
-for i in range(len(q_ap_series)):
+for i in range(len(alpha_ap_series)):
     matrixI_i = matrixI[:, i]
-    half_steps = int(simulating_steps/2)
-    I_min = matrixI_i[half_steps]
-    for j in range(half_steps):
-        if matrixI_i[half_steps+j] <= I_min:
-            I_min = matrixI_i[half_steps+j]
-            idx_min = half_steps+j
-        else:
-            break
+    
+    if object_type == "Step amplitude object" or object_type == "Error function amplitude object":
+        half_steps = int(simulating_steps/2)
+        # Starting from the centre and find the local minimum to the right of the central point
+        I_min = matrixI_i[half_steps]
+        for j in range(1, half_steps):
+            if matrixI_i[half_steps+j] < I_min:
+                I_min = matrixI_i[half_steps+j]
+            else:
+                idx_min = half_steps+j-1
+                break
+            
+        # Starting from the centre and find the local maximum to the left of the central point
+        I_max = matrixI_i[half_steps]
+        for j in range(1, half_steps):
+            if matrixI_i[half_steps-j] > I_max:
+                I_max = matrixI_i[half_steps-j]            
+            else:
+                idx_max = half_steps-j+1
+                break
+            
         
-    I_max = matrixI_i[half_steps]
-    for j in range(half_steps):
-        if matrixI_i[half_steps-j] >= I_max:
-            I_max = matrixI_i[half_steps-j]
-            idx_max = half_steps-j
-        else:
-            break
+        # The region of interest to find the resolution
+        x_array_focus = x_array[idx_max:idx_min]
+        matrixI_focus = matrixI_i[idx_max:idx_min]
+        
+        I_100_index = np.argmin(np.abs(matrixI_focus - 1))
+        I_100 = matrixI_focus[I_100_index]
+        I_0_index = np.argmin(np.abs(matrixI_focus - 1/2))
+        I_0 = matrixI_focus[I_0_index]
+        
+        I_84 = I_0 + (I_100 - I_0)*84/100
+        I_16 = I_0 + (I_100 - I_0)*16/100
+        
+        I_84_index = np.argmin(np.abs(matrixI_focus - I_84))
+        x_84 = x_array_focus[I_84_index]
+        I_16_index = np.argmin(np.abs(matrixI_focus - I_16))
+        x_16 = x_array_focus[I_16_index]
+        resolution_i = x_16 - x_84
     
-    # The region of interest to find the resolution
-    x_array_focus = x_array[idx_max:idx_min]
-    matrixI_i_focus = matrixI_i[idx_max:idx_min]
+    if object_type == "Step phase object" or object_type == "Error function phase object":
+        # Finding the local minimum around the central point
+        half_steps = int(simulating_steps/2)
+        I_min = matrixI_i[half_steps]
+        for j in range(1, half_steps):
+            if matrixI_i[half_steps+j] < I_min:
+                I_min = matrixI_i[half_steps+j]
+            else:
+                idx_min = half_steps+j-1
+                break
+        
+        current_min_idx = idx_min
+        for j in range(1, half_steps):
+            if matrixI_i[current_min_idx-j] < I_min:
+                I_min = matrixI_i[current_min_idx-j]
+            else:
+                idx_min = current_min_idx-j+1
+                break
+        
+        # Finding the local maximum to the right of this minimum
+        I_right = matrixI_i[idx_min]
+        for j in range(1, half_steps):
+            if matrixI_i[idx_min+j] > I_right:
+                I_right = matrixI_i[idx_min+j]
+            else:
+                idx_right = idx_min+j-1
+                break
+            
+        # Finding the local maximum to the left of this minimum
+        I_left = matrixI_i[idx_min]
+        for j in range(1, half_steps):
+            if matrixI_i[idx_min-j] > I_left:
+                I_left = matrixI_i[idx_min-j]            
+            else:
+                idx_left = idx_min-j+1
+                break
+            
+        # The dip is counted from the lower value between I_left and I_right
+        if I_left > I_right:
+            idx_left = idx_left + np.argmin(np.abs(matrixI_i[idx_left:idx_min] - I_right))
+            I_left = matrixI_i[idx_left]
+        elif I_left < I_right:
+            idx_right = idx_min + np.argmin(np.abs(matrixI_i[idx_min:idx_right] - I_left))
+            I_right = matrixI_i[idx_right]    
+            
+        # Shifting the left and right points of consideration to those of value 1
+        I_left_index = np.argmin(np.abs(matrixI_i[idx_left:idx_min] - 1))
+        I_left = matrixI_i[idx_left]
+        I_right_index = np.argmin(np.abs(matrixI_i[idx_min:idx_right] - 1))
+        I_right = matrixI_i[idx_right]
+        
+        I_50left = I_min + (I_left - I_min)/2
+        I_50right = I_min + (I_right - I_min)/2
+        
+        I_50left_index = idx_left + np.argmin(np.abs(matrixI_i[idx_left:idx_min] - I_50left))
+        x_50left = x_array[I_50left_index]
+        I_50right_index = idx_min + np.argmin(np.abs(matrixI_i[idx_min:idx_right] - I_50right))
+        x_50right = x_array[I_50right_index]
+        resolution_i = x_50right - x_50left
+        
+        
+    resolution_list.append(resolution_i)
     
-    I_84 = I_min + (I_max - I_min)*84/100
-    I_16 = I_min + (I_max - I_min)*16/100
-    
-    I_84_index = np.where(np.abs(matrixI_i_focus - I_84) == min(np.abs(matrixI_i_focus - I_84)))
-    x_84 = x_array_focus[I_84_index[0]]
-    I_16_index = np.where(np.abs(matrixI_i_focus - I_16) == min(np.abs(matrixI_i_focus - I_16)))
-    x_16 = x_array_focus[I_16_index[0]]
-    resolution_i = x_16 - x_84
-    resolution_list.append(resolution_i[0])
-    
-plt.plot(alpha_ap_series, resolution_list)
+plt.plot(alpha_ap_series*1e3, resolution_list)
+# naming the x axis
+plt.xlabel('Aperture angle (mrad)')
+# naming the y axis
+plt.ylabel('Resolution (nm)')
+  
+# giving a title to my graph
+plt.title('R(alpha_ap)')
+
+plt.show()
 
 # Save this list of resolution into a csv file
-with open('resolution_aperture_IBMnac.csv', 'a') as csvfile:
+if aberration_corrected == False:
+    filename = object_type + '_R(a)_IBM_nac.csv' 
+if aberration_corrected == True:
+    filename = object_type + '_R(a)_IBM_ac.csv'
+
+with open(filename, 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
-    writer.writerow(['aperture_angle (mrad)', 'resolution (nm)'])
+    writer.writerow(['Aperture angle (mrad)', 'Resolution (nm)'])
     
     for i in range(len(alpha_ap_series)):
-        writer.writerow([round(1e3 * alpha_ap_series[i], 5), round(1e9 * resolution_list[i], 5)])
+        writer.writerow([round(alpha_ap_series[i], 3), round(1e9 * resolution_list[i], 10)])
  
     csvfile.close()
 
-'''
+
 # Plotting the object
 plt.plot(x_array, object_amplitude)
 plt.plot(x_array, object_phase)
-
 # plotting the curves
 for i in range(len(alpha_ap_series)):
     plt.plot(x_array, matrixI[:, i])
     
-    plt.xlim(-10e-9, 10e-9)
-
+    plt.xlim(-20e-9, 20e-9)
     # naming the x axis
     plt.xlabel('Position x (m)')
     # naming the y axis
@@ -331,8 +426,51 @@ for i in range(len(alpha_ap_series)):
     plt.title('I(x)')
     
     plt.show()
+    
+'''    
+# The programme to plot
+import matplotlib.pyplot as plt
+import csv
+a_array_ac = []
+a_array_nac = []
+R_ac = []
+R_nac = []
+with open('Step amplitude object_R(E_0)_IBM_ac.csv', 'r') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+        else:
+            if row == []:
+                continue
+            else:
+                a_array_ac.append(float(row[0]))
+                R_ac.append(float(row[1]))    
+    csvfile.close()
+with open('Step amplitude object_R(E_0)_IBMnac.csv', 'r') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+        else:
+            if row == []:
+                continue
+            else:
+                a_array_nac.append(float(row[0]))
+                R_nac.append(float(row[1]))   
+    csvfile.close()
+    
+########## Plotting the curves ############
+plt.plot(a_array_ac*1e3, R_ac, label = "ac")
+plt.plot(a_array_nac*1e3, R_nac, label = "nac")
+plt.xlabel("Aperture angle (mrad)")
+plt.ylabel("Resolution (nm)")
+plt.title("Resolution versus aperture angle")
+plt.legend()
+plt.show()
 '''
 ################################
 ###### End of Programme ########
 ################################
-
