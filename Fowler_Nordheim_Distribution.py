c89de3212fd83490aaa9562583b52f6987b51d6e
ntan242001 <62791920+ntan242001@users.noreply.github.com>
2021-07-21 16:14:12 +0800

    Update Fowler_Nordheim_Distribution.py

--- Fowler_Nordheim_Distribution.py
+++ Fowler_Nordheim_Distribution.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+### Generating the Fowler-Nordheim distribution ### 
 k_B = 1.38*1e-23
 hbar = 1.055*1e-34
 m = 9.1*1e-31
@@ -12,26 +13,107 @@ E_F = 5.77   # eV
 # Temperature
 T = 300      # K
 # Electric field
-F = 4.5*1e9
+F = 4.5*1e9   # V/m
 
 d_0_inv = 2*np.sqrt(2*phi*e*m)/(e*F*hbar) 
 
 # An array of electron energy in eV 
-E_array = np.linspace(4.6, 6, 100)
+E_array = np.linspace(4.6, 6, 1000)
 
 # Fermi-Dirac distribution
 f_E = 1/(np.exp((E_array-E_F)*e/(k_B*T)) + 1)
 
 counts = (np.exp(E_array*e*d_0_inv) - 1)*f_E
 
-plt.plot(E_array - E_F, counts)
-plt.axvline(x=0, color = 'k')
+idx_max = np.argmax(counts)
+counts_max = counts[idx_max]
 
+counts_half = counts_max/2
+
+left_index = np.argmin(np.abs(counts[0:idx_max] - counts_half))
+right_index = idx_max + np.argmin(np.abs(counts[idx_max:-1] - counts_half))
+
+G_FWHM = E_array[right_index] - E_array[left_index]
+
+################################
+
+
+N = 3
+
+def gauss(x, A, x0, sigma):
+    return A * np.exp(-((x - x0))**2/(2*sigma**2))
+
+def N_gauss(N, x, A, x0, sigma):
+    N_gauss = 0
+    for i in range(N):
+        N_gauss += gauss(x, A[i], x0[i], sigma[i])
+    return N_gauss
+
+A = [5.487*1e11, 1.691*1e11, 4.031*1e11]
+x0 = [-0.03925, -0.3382, -0.1438]
+c = [0.07507, 0.2815, 0.136]
+
+sigma = c/np.sqrt(2)
+FWHM = 2*np.sqrt(2*np.log(2)) * sigma
+
+mu = []
+for i in range(N):
+    mu.append(A[i]*(np.sqrt(2*np.pi*sigma[i]**2)) )
+
+mu_tot = sum(mu)
+mu = mu/mu_tot
+
+
+########## Plotting the curve ############
+plt.plot(E_array - E_F, counts, 'r',label = 'FN distribution')
+plt.plot(E_array - E_F, N_gauss(N, E_array - E_F, A, x0, sigma), 'b', label='Triple Gaussian fit')
+
+plt.axvline(x=0, linestyle = '--' ,color = 'k')
+
+color = ['c', 'g', 'm', 'p']
+for i in range(N):
+    if i == 0:
+        label = '1st Gaussian'
+    if i == 1:
+        label = '2nd Gaussian'    
+    if i == 2:
+        label = '3rd Gaussian'
+    if i == 4:
+        label = '4th Gaussian'
+    plt.plot(E_array - E_F, gauss(E_array - E_F, A[i], x0[i], sigma[i]), color = color[i], label=label)
+
+
+#plt.xlim(-1.3, 1.8)
+#plt.ylim(0, 175)
+# naming the x axis
+plt.xlabel('Energy (eV)')
+# naming the y axis
 plt.ylabel('Counts')
-plt.xlabel('Electron energy (eV)')
-
+# giving a title to my graph
+plt.title('Field emission distribution')
 plt.text(-0.07, 0, '$E_F$')
-print(list(counts))
+plt.legend()
+
+plt.show()
+
+for i in range(N):
+    if i == 0:
+        label = '1st Gaussian'
+    if i == 1:
+        label = '2nd Gaussian'    
+    if i == 2:
+        label = '3rd Gaussian'
+    if i == 4:
+        label = '4th Gaussian'
+    print(label)    
+    print('Weight: ' + str(round(mu[i], 5)))
+    print('Norminal energy: ' + str(round(x0[i], 5)) + ' eV')
+    print('FWHM = ' +  str(round(FWHM[i], 4)) + ' eV; sigma = ' + str(round(sigma[i], 4)))
+    print()
+    
+print('Single Gauss fit FWHM:', round(G_FWHM, 4))
+#print(list(counts))
+
 
 '''
 General model Gauss3:
