fee8d285462ae4bb8c191d6e4d90dbacad51fd7f
ntan242001 <62791920+ntan242001@users.noreply.github.com>
2021-07-21 14:04:16 +0800

    Update Fowler_Nordheim_Distribution.py

--- Fowler_Nordheim_Distribution.py
+++ Fowler_Nordheim_Distribution.py
@@ -32,3 +32,26 @@ plt.xlabel('Electron energy (eV)')
 
 plt.text(-0.07, 0, '$E_F$')
 print(list(counts))
+
+'''
+General model Gauss3:
+     f(x) = 
+              a1*exp(-((x-b1)/c1)^2) + a2*exp(-((x-b2)/c2)^2) + 
+              a3*exp(-((x-b3)/c3)^2)
+Coefficients (with 95% confidence bounds):
+       a1 =   5.487e+11  (3.847e+11, 7.127e+11)
+       b1 =    -0.03925  (-0.04167, -0.03684)
+       c1 =     0.07507  (0.06672, 0.08342)
+       a2 =   1.691e+11  (1.329e+11, 2.052e+11)
+       b2 =     -0.3382  (-0.4119, -0.2646)
+       c2 =      0.2815  (0.2384, 0.3247)
+       a3 =   4.031e+11  (2.726e+11, 5.336e+11)
+       b3 =     -0.1438  (-0.1771, -0.1104)
+       c3 =       0.136  (0.1019, 0.1701)
+
+Goodness of fit:
+  SSE: 1.176e+22
+  R-square: 0.9979
+  Adjusted R-square: 0.9978
+  RMSE: 1.137e+10
+'''
