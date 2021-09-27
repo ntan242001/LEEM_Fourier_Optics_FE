2ba977a3e721b66cf0c2039e9debde2a1633eb59
ntan242001 <62791920+ntan242001@users.noreply.github.com>
2021-09-27 21:45:11 +0800

    Create Envelop_shift.py

---
+++ Envelop_shift.py
@@ -0,0 +1,206 @@
+# Comparing the single and double Gaussian distributions for different aperture angles
+import numpy as np
+import matplotlib.pyplot as plt
+from joblib import Parallel, delayed
+import time
+import math
+
+##############################
+######### Preamble ###########
+##############################
+
+
+# A function to choose different LEEM parameters
+def choose_LEEM_type(LEEM_type_str, aberration_corrected_bool = False):
+    global E, E_0, C_c, C_cc, C_3c, C_3, C_5, alpha_ap, alpha_ill, \
+        delta_E, M_L, lamda, lamda_0, q_ill, q_ap, LEEM_type, aberration_corrected
+    LEEM_type = LEEM_type_str
+    aberration_corrected = aberration_corrected_bool
+    
+    if LEEM_type == "IBM":
+        if aberration_corrected == False:
+            E = 15010  # eV  Nominal Energy After Acceleration
+            E_0 = 10  # eV  Energy at the sample
+            
+            C_c = -0.075  # m  Second Rank Chromatic Aberration Coefficient
+            C_cc = 23.09 # m   Third Rank Chromatic Aberration Coefficient
+            C_3c = -59.37  # m   Forth Rank Chromatic Aberration Coefficient
+            
+            C_3 = 0.345  # m  Third Order Spherical Aberration Coefficient
+            C_5 = 39.4  # m  Fifth Order Spherical Aberration Coefficient
+            
+            alpha_ap = 2.34e-3  # rad Aperture angle
+            alpha_ill = 0.1e-3  # rad Illumination Divergence Angle
+            
+            delta_E = 0.25  # eV  Energy Spread
+            M_L = 0.653  # Lateral Magnification
+            print("IBM nac chosen.")
+            
+        elif aberration_corrected == True:
+            E = 15010  # eV  Nominal Energy After Acceleration
+            E_0 = 10  # eV  Energy at the sample
+            
+            C_c = 0  # m   Second Rank Chromatic Aberration Coefficient
+            C_cc = 27.9 # m   Third Rank Chromatic Aberration Coefficient
+            C_3c = -67.4 # m   Forth Rank Chromatic Aberration Coefficient
+            
+            C_3 = 0  # m   Spherical Aberration Coefficient
+            C_5 = 92.8
+        
+            alpha_ap = 7.37e-3  # rad Aperture angle
+            alpha_ill = 0.1e-3  # rad Illumination Divergence Angle
+        
+            delta_E = 0.25  # eV  Energy Spread
+            M_L = 0.653  # Lateral Magnification
+            print("IBM ac chosen.")
+            
+        lamda = 6.6261e-34 / np.sqrt(2 * 1.6022e-19 * 9.1095e-31 * E)
+        lamda_0 = 6.6261e-34 / np.sqrt(2 * 1.6022e-19 * 9.1095e-31 * E_0)
+    
+        q_ap = alpha_ap/lamda
+        q_ill = alpha_ill/lamda
+        
+    elif LEEM_type == "Energy dependent":
+        if aberration_corrected == False:
+            E = 15010  # eV  Nominal Energy After Acceleration
+            E_0 = 20 # eV  Energy at the sample ##########CUSTOMIZABLE INPUT##########
+            kappa = np.sqrt(E/E_0)
+            
+            C_c = -0.0121 * kappa**(1/2) + 0.0029 # m  Second Rank Chromatic Aberration Coefficient
+            C_cc = 0.5918 * kappa**(3/2) - 87.063 # m   Third Rank Chromatic Aberration Coefficient
+            C_3c = -1.2141 * kappa**(3/2) + 169.41  # m   Forth Rank Chromatic Aberration Coefficient
+            
+            C_3 = 0.0297 * kappa**(1/2) + 0.1626  # m  Third Order Spherical Aberration Coefficient
+            C_5 = 0.6223 * kappa**(3/2) - 79.305  # m  Fifth Order Spherical Aberration Coefficient
+            
+            delta_E = 0.25  # eV  Energy Spread
+            alpha_ill = 0.1e-3  # rad Illumination divergence angle
+            M_L = 0.653  # Lateral Magnification
+            
+            lamda = 6.6261e-34 / np.sqrt(2 * 1.6022e-19 * 9.1095e-31 * E) # in metre
+            alpha_ap = (lamda/C_3)**(1/4) # rad Aperture angle for optimal resolution
+            
+            print("Custom nac LEEM at E_0 = " + str(E_0) + " eV chosen.")
+            
+        if aberration_corrected == True:
+            E = 15010  # eV  Nominal Energy After Acceleration
+            E_0 = 20 # eV  Energy at the sample
+            kappa = np.sqrt(E/E_0)
+            
+            C_c = 0 # m  Second Rank Chromatic Aberration Coefficient
+            C_cc = 0.5984 * kappa**(3/2) - 84.002 # m   Third Rank Chromatic Aberration Coefficient 
+            C_3c = -1.1652 * kappa**(3/2) + 153.58  # m   Forth Rank Chromatic Aberration Coefficient  
+            
+            C_3 = 0  # m  Third Order Spherical Aberration Coefficient
+            C_5 = 0.5624 * kappa**(3/2) - 16.541  # m  Fifth Order Spherical Aberration Coefficient
+            
+            delta_E = 0.25  # eV  Energy Spread
+            alpha_ill = 0.1e-3  # rad Illumination divergence angle
+            M_L = 0.653  # Lateral Magnification
+            
+            lamda = 6.6261e-34 / np.sqrt(2 * 1.6022e-19 * 9.1095e-31 * E) # in metre
+            alpha_ap = (3/2*lamda/C_5)**(1/6) # rad Aperture angle for optimal resolution
+            
+            print("Custom ac LEEM at E_0 = " + str(E_0) + " eV chosen.")   
+        
+        lamda_0 = 6.6261e-34 / np.sqrt(2 * 1.6022e-19 * 9.1095e-31 * E_0) # in metre
+        
+        q_ap = alpha_ap/lamda
+        q_ill = alpha_ill/lamda
+
+
+# A function to set different defocus values
+def choose_defocus(defocus_type, value = 0):
+    global delta_z
+    if defocus_type == "In-focus":
+        delta_z = 0
+        print("In-focus chosen.")
+    elif defocus_type == "Scherzer defocus":
+        delta_z = np.sqrt(3/2*C_3*lamda)
+        print("Scherzer defocus chosen.")
+    elif defocus_type == "A-Phi Scherzer defocus":
+        delta_z = np.sqrt(9/64*C_5*lamda**2)
+        print("A-Phi Scherzer defocus chosen.")
+    elif defocus_type == "custom":
+        delta_z = value
+
+
+object_size = 400               # simulating object size in nm
+simulating_steps = 1 + 2**13 # total simulating steps
+# An array of points in the x space
+x_array = (np.linspace(-object_size/2, object_size/2, simulating_steps) + object_size/simulating_steps)*1e-9
+
+
+
+choose_LEEM_type("IBM", aberration_corrected_bool = False)
+choose_defocus("In-focus")
+
+##################################
+######## End of Preamble #########
+##################################
+
+
+
+##################################
+########### Main Part ############
+##################################
+t_0 = time.time()
+print("Simulation start.")
+# # The object image is reversed through the lens
+# object_function_reversed = object_function[::-1] 
+
+epsilon_0_series = np.linspace(-0.5, -0.5, 11)
+
+# Initialising the series of function I_1(x) and I_2(x) at different values of q_ap
+matrixE_ct = np.zeros((len(x_array), len(epsilon_0_series)), dtype=complex)
+
+q = 1 / (simulating_steps* (x_array[1] - x_array[0])) * np.arange(0, simulating_steps, 1)
+
+# Shifting the q array to centre at 0 
+q = q - np.max(q) / 2
+
+# A function to calculate the image for single Gaussian distribution
+def Envelop1Gauss(epsilon_0, epsilon_0_index):
+    # Arrays for the calculation of the double integration 
+    Q, QQ = np.meshgrid(q, q)
+    
+    delta_E = 0.2424 # eV
+    sigma_E = delta_E/(2*np.sqrt(2*np.log(2)))
+    b_1 = 1/2*C_c*lamda*(Q**2 - QQ**2)/E + 1/4*C_3c*lamda**3*(Q**4 - QQ**4)/E
+    b_2 = 1/2*C_cc*lamda*(Q**2 - QQ**2)/E**2
+    
+    # The purely chromatic envelop functions
+    E_cc = (1 - 1j*4*np.pi*b_2*sigma_E**2)**(-1/2)
+    E_ct = E_cc * np.exp(-2*np.pi**2 *E_cc**2 *sigma_E**2 *b_1**2)
+    
+    return E_ct[int(len(q)/2),:]
+
+with Parallel(n_jobs=-1, verbose=50, max_nbytes="50M") as parallel:
+    parallelResult = parallel(delayed(Envelop1Gauss)(epsilon_0, epsilon_0_index) for epsilon_0, epsilon_0_index in enumerate(epsilon_0_series))
+
+for mat in parallelResult:
+    matrixE_ct = np.hstack(matrixE_ct, mat)
+
+for i in range(len(epsilon_0_series)):
+    E_ctot = matrixE_ct[:,i]
+
+    plt.plot(q/(1e9), E_ctot.real, label = 'Re($E_{C,tot}(q,0)$)')
+    plt.plot(q/(1e9), E_ctot.imag, label = 'Im($E_{C,tot}(q,0)$)')
+    
+    
+    plt.xlim(-0.7, 0.7)
+    # naming the x axis
+    plt.xlabel('q($nm^{-1}$)')
+    # naming the y axis
+    plt.ylabel('$E_{C,tot}(q,0)$')
+      
+    # giving a title to my graph
+    plt.title('$$E_{ct,tot}(q,0)$ versus q, epsilon_0 = ' + str(epsilon_0_series[i]) )
+    plt.legend()
+    
+    plt.show()
+
+
+################################
+###### End of Programme ########
+################################
